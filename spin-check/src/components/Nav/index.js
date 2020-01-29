6edac476eb20cb8e83f86fe8cb2f989570c2acc3
Your Name <alexsigala87@gmail.com>
2020-01-29 10:24:38 -0800

    added html routes to the nav bar. will work on making aPI calls to news API next

--- spin-check/src/components/Nav/index.js
+++ spin-check/src/components/Nav/index.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import "./style.css";
 import Nav from "react-bootstrap/Nav";
+
 class NavBar extends Component {
     render() {
         return (
-            <Nav
-                activeKey="/home"
-                onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-            >
-                <Nav.Item>
-                    <Nav.Link href="/home">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Trending</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2">Search The Globe</Nav.Link>
-                </Nav.Item>
-            </Nav>
+            <div>
+                <Nav activeKey="/home">
+                    <Nav.Item>
+                        <Nav.Link href="/home">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/trending">Trending</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/search">Search The Globe</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
         );
     }
 }
