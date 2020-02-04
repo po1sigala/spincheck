dc43471b1e8511bb366a5eecf382f997bee34e69
Your Name <alexsigala87@gmail.com>
2020-02-04 00:32:38 -0800

    basic functionality built for trending. searches top five headlines by country. will add functionality to make selections for which countries you want to see headlines from

--- spin-check/src/pages/Trending.js
+++ spin-check/src/pages/Trending.js
@@ -1,11 +1,50 @@
 import React, { Component } from "react";
 
 import Article from "../components/Article";
-import SearchBar from "../components/searchBar";
-import SitesList from "../components/SiteList";
+
 import Jumbotron from "../components/Jumbotron";
+import API from "../Utils/API";
+
 class Trending extends Component {
+    //default to returning 5 trending articles
+    state = {
+        numCards: 5,
+        countries: ["us"],
+        responses: []
+    };
+    componentDidMount = () => {
+        const newState = [];
+        this.state.countries.map(country => {
+            API.TrendingHeadlines(country).then(res => {
+                const results = res.data.articles;
+                console.log("results are");
+                console.log(results);
+                newState.push(results);
+                this.setState({ responses: newState });
+            });
+        });
+        console.log(this.state.articles);
+    };
     render() {
+        const APIResponses = this.state.responses;
+        console.log("responses are ");
+        console.log(APIResponses);
+
+        let i = 0;
+
+        const carousels = APIResponses.map(setOfArticles => {
+            i++;
+
+            return (
+                <Article
+                    key={i}
+                    articles={setOfArticles}
+                    numCards={this.state.numCards}
+                    id={this.state.countries[i]}
+                    maximum={this.state.maximum}
+                ></Article>
+            );
+        });
         return (
             <div>
                 <div>
@@ -15,10 +54,11 @@ class Trending extends Component {
                     />
                 </div>
                 <div className="row" id="contentRow">
-                    <div className="infocolumn col-md-6">
-                        <div>
-                            <SearchBar />
-                        </div>
+                    <div className="infocolumn col-md-3 col-sm-3 col-lg-3">
+                        TOP 5 USA
+                    </div>
+                    <div className="col-md-9 col-lg-9 col-sm-9">
+                        {carousels}
                     </div>
                 </div>
             </div>
