637b76b289c1ae2fe11ef11aefbabd882fadaad2
Your Name <alexsigala87@gmail.com>
2020-02-05 23:43:01 -0800

    Updates

---
+++ precache-manifest.98f80b767f53ba4a820bcb54d77a14a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e073d3b76d45c62701ab30af4d52a2",
+    "url": "/spincheck/index.html"
+  },
+  {
+    "revision": "bd1cfda8143cfa32cdc7",
+    "url": "/spincheck/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a6080afb6f6edaf0db5d",
+    "url": "/spincheck/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "bd1cfda8143cfa32cdc7",
+    "url": "/spincheck/static/js/2.f90c4947.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/spincheck/static/js/2.f90c4947.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6080afb6f6edaf0db5d",
+    "url": "/spincheck/static/js/main.7487432f.chunk.js"
+  },
+  {
+    "revision": "0b6587f6aad1dd3eaf68",
+    "url": "/spincheck/static/js/runtime-main.e141693e.js"
+  },
+  {
+    "revision": "92914b3ecaf0cb8e22009fccb610744e",
+    "url": "/spincheck/static/media/black.92914b3e.jpg"
+  }
+]);
